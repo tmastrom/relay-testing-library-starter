2fea44dd9f9b6ab1a5596e97f94df4827a20e0ed
Brianna Cerkiewicz <brianna@button.is>
2022-02-28 11:51:01 -0800

    try to fetch from GraphQL (sans Relay)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+const { useState, useEffect } = React;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// We'll load the name of a repository, initially setting it to null
+const [query, setQuery] = useState(null);
+
+// When the component mounts we'll fetch a repository name
+useEffect(() => {
+  let isMounted = true;
+  fetchGraphQL(`
+  query MyQuery {
+    query {
+      allTodos {
+        edges {
+          node {
+            id
+            dateUpdated
+            dateCreated
+            completed
+            task
+          }
+        }
+      }
+    }
+  }
+  `).then(response => {
+    // Avoid updating state if the component unmounted before the fetch completes
+    if (!isMounted) {
+      return;
+    }
+    const data = response.data;
+    setQuery(data);
+    console.log('data is',data)
+  }).catch(error => {
+    console.error(error);
+  });
+
+  return () => {
+    isMounted = false;
+  };
+}, []);
+
+// Render "Loading" until the query completes
+return (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        {query != null ? `Query: ${query}` : "Loading"}
+      </p>
+    </header>
+  </div>
+);
 }
 
 export default App;
