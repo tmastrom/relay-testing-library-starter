1be590edee067bf0e7bd2b1e49ecae3b7f300b5f
Brianna Cerkiewicz <brianna@button.is>
2022-02-28 13:57:56 -0800

    create fetchgraphql function

--- client/src/fetchGraphQL.js
+++ client/src/fetchGraphQL.js
@@ -1,8 +1,8 @@
 async function fetchGraphQL(text, variables) {
-    const response = await fetch('http://localhost/5000/graphql', {
+    const response = await fetch('http://localhost:5000/graphql', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         query: text,
