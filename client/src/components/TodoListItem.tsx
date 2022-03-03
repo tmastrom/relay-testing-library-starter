854d16dc73f72e1a4ad78f6713a9d036c79bd603
Brianna Cerkiewicz <brianna@button.is>
2022-03-03 13:52:15 -0800

    persist completed status

--- client/src/components/TodoListItem.tsx
+++ client/src/components/TodoListItem.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import '../App.css';
 import {graphql} from 'babel-plugin-relay/macro';
-import { useFragment } from 'react-relay/hooks';
+import { useFragment, commitMutation } from 'react-relay/hooks';
 import type {TodoListItem_todo$key} from './__generated__/TodoListItem_todo.graphql';
 import Checkbox from '@button-inc/component-library/Checkbox'
+import RelayEnvironment from '../RelayEnvironment';
 
 type Props = {
     //$key includes data and fragment reference
     todo: TodoListItem_todo$key
 }
+
+const UpdateTodoMutation = graphql`
+mutation TodoListItemUpdateMutation (
+  $input: UpdateTodoInput!) {
+  updateTodo(input: $input) {
+    todoEdge {
+      node {
+        completed
+      }
+    }
+  }
+}
+
+
+`;
 
 function TodoListItem(props: Props) {
     const data = useFragment(
@@ -21,9 +37,30 @@ function TodoListItem(props: Props) {
         `,
         props.todo,
       );
+
+      const handleClick = e => {
+        const change = e.target.checked;
+
+        commitMutation(RelayEnvironment, {
+          mutation: UpdateTodoMutation,
+          variables: {input: {
+            id: data.id,
+            todoPatch: {
+              completed: change
+            }
+          }},
+          onCompleted: response => {
+            console.log('completed, response is:',response)
+          } /* Mutation completed */,
+          onError: error => {
+            console.log('error is:',error)
+          } /* Mutation errored */,
+        });
+
+      }
+
       return (
-          <Checkbox className={"checkbox"} label={data.task} defaultChecked={data.completed}/>
-          //brianna--add onchange
+          <Checkbox className={"checkbox"} label={data.task} defaultChecked={data.completed} onClick={handleClick}/>
       )
       
 
