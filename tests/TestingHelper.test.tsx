ba82138a0512ffa52ce9ada37639d86dab13d5b2
Brianna Cerkiewicz <brianna@button.is>
2022-10-14 13:20:49 -0700

    chore: spec work on expectMutationToBeCalled test

--- tests/TestingHelper.test.tsx
+++ tests/TestingHelper.test.tsx
@@ -1,5 +1,7 @@
-import { act, screen } from "@testing-library/react";
 import TestingHelper from "../src/TestingHelper";
+import { commitMutation, graphql } from "react-relay";
+import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils";
+import ReactTestRenderer from "react-test-renderer";
 
 describe("TestingHelper", () => {
   beforeEach(() => {
@@ -24,7 +26,66 @@ describe("TestingHelper", () => {
     );
   });
 
-  it("expectMutationToBeCalled function returns a message when the expected mutation is not called", () => {});
+  it("expectMutationToBeCalled function returns a message when the expected mutation is not called", () => {
+    const testingHelper = new TestingHelper();
+
+    // brianna--how to set this up?
+    const mutation = graphql`
+      mutation addEmissionIntensityReportToRevisionMutation(
+        $input: AddEmissionIntensityReportToRevisionInput!
+      ) {
+        addEmissionIntensityReportToRevision(input: $input) {
+          formChanges {
+            id
+            rowId
+            asEmissionIntensityReport {
+              calculatedEiPerformance
+            }
+            newFormData
+            projectRevisionByProjectRevisionId {
+              ...TaskList_projectRevision
+              ...ProjectEmissionIntensityReportForm_projectRevision
+            }
+          }
+        }
+      }
+    `;
+
+    function sendMutation(environment, onCompleted, onError, variables) {
+      commitMutation(environment, {
+        mutation: mutation,
+        onCompleted,
+        onError,
+        variables,
+      });
+    }
+
+    const environment = createMockEnvironment();
+    const onCompleted = jest.fn();
+    sendMutation(environment, onCompleted, jest.fn(), {});
+    // const operation = environment.mock.getMostRecentOperation();
+    // const operationName =
+    //   environment.mock.getMostRecentOperation().fragment.node.name;
+
+    // brianna--best way to test this is to compare the strings?
+    const expected =
+      `Expected mutation testMutation to be called. Mutations called:\n` +
+      `addEmissionIntensityReportToRevisionMutation`;
+
+    const result = testingHelper.expectMutationToBeCalled("testMutation", {}); // brianna--does this actually return the error or do we access it another way
+
+    expect(result).toEqual(expected);
+
+    // resolve the mutation--I don't think this matters for the helper
+    // ReactTestRenderer.act(() => {
+    //   environment.mock.resolve(
+    //     operation,
+    //     MockPayloadGenerator.generate(operation)
+    //   );
+    // });
+
+    // expect(onCompleted).toBeCalled();
+  });
 
   it("expectMutationToBeCalled function returns a message when the expected variables are not returned", () => {});
 });
