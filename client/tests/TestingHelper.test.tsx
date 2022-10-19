ce5f5d74d1359d4af718ffb2314bc36a1e1e90f3
Brianna Cerkiewicz <brianna@button.is>
2022-10-19 14:54:07 -0700

    test: test TestingHelper.tsx

---
+++ client/tests/TestingHelper.test.tsx
@@ -0,0 +1,64 @@
+import TestingHelper from "./TestingHelper";
+import { commitMutation } from "react-relay";
+import CreateTodoMutation from "../src/components/__generated__/CreateTodoMutation.graphql";
+
+describe("TestingHelper", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("reinit function creates an environment, errorContext, and mock router", () => {
+    const testingHelper = new TestingHelper();
+    testingHelper.reinit();
+    expect(testingHelper.environment).toEqual(expect.anything());
+    expect(testingHelper.expectMutationToBeCalled).toEqual(
+      expect.any(Function)
+    );
+    expect(testingHelper.reinit).toEqual(expect.any(Function));
+  });
+
+  it("expectMutationToBeCalled function returns a message when the expected mutation is not called", () => {
+    const testingHelper = new TestingHelper();
+    testingHelper.reinit();
+
+    commitMutation(testingHelper.environment, {
+      mutation: CreateTodoMutation,
+      variables: {},
+    });
+
+    expect(() => {
+      testingHelper.expectMutationToBeCalled("testMutation", {});
+    }).toThrow(
+      `Expected mutation testMutation to be called. Mutations called:\n` +
+        `CreateTodoMutation`
+    );
+  });
+
+  it("expectMutationToBeCalled function returns a message when the expected variables are not returned", () => {
+    const testingHelper = new TestingHelper();
+    testingHelper.reinit();
+    const variables = commitMutation(testingHelper.environment, {
+      mutation: CreateTodoMutation,
+      variables: { connections: "test", input: "test" },
+    });
+
+    expect(() => {
+      testingHelper.expectMutationToBeCalled("CreateTodoMutation", {
+        test: "iwillfail",
+      });
+    }).toThrow(
+      `Expected mutation CreateTodoMutation to be called with:` +
+        `
+{
+  "test": "iwillfail"
+}` +
+        `
+received:
+` +
+        `{
+  "connections": "test",
+  "input": "test"
+}`
+    );
+  });
+});
